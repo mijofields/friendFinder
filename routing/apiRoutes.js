3f2364954b10662b93f97d2d8fcd621172193172
El Pirata Mike <mikefields81@gmail.com>
2018-05-18 13:01:26 -0500

    removing friends/api get method

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -9,26 +9,17 @@ var User = friends.User;
 
 
 module.exports = function(app){
-	app.get('/api/friends', function(req, res){
-		res.json(friendsArr);
-	});
+	// app.get('/api/friends', function(req, res){
+	// 	res.json(friendsArr);
+	// });
 
 
 	app.post('/api/friends', function(req, res) {
 
 
-		// console.log("req works");
-		// console.log(req.body);
-		// console.log("------------------------------");
-
 		var user =  new User (req.body.name, req.body.species, req.body.scores);
 		
 		user.match();
-		// console.log("user bestie");
-		// console.log(user);
-		// console.log("-------------------------------");
-
-		// console.log(user.bestie);
 		res.json(user.bestie); 	
 		return;
 		
