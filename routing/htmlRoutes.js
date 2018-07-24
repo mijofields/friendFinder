12919fcb010117eaf810b7913eb004fa612a8c96
El Pirata Mike <mikefields81@gmail.com>
2018-07-24 12:53:24 -0500

    adding post to html routes

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -18,9 +18,12 @@ module.exports = function(app){
 
 	app.get('/survey', function(req, res){
 		res.sendFile(path.join(__dirname, '../public/survey.html'));
+
 	});
 
-	
+	app.post('/survey', function(req, res){
+			res.sendFile(path.join(__dirname, '../public/survey.html'));
+	});
 
 
 };
