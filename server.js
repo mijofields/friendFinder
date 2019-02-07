a5a3c09a63673fc9355e22eacfedaa361711c6de
El Pirata Mike <mikefields81@gmail.com>
2019-02-07 10:39:31 -0600

    adding function to ping heroku so app doesn't idle

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const http = require("http");
+
 
 const app = express();
 const port = process.env.PORT || 4812;
@@ -23,3 +25,11 @@ require("./routing/htmlRoutes")(app);
 app.listen(port, function() {
   console.log("App listening on PORT: " + port);
 });
+
+//setInterval to ping the app every 5 mins to prevent it going idle
+
+setInterval(function() {
+
+    http.get("https://tranquil-thicket-86715.herokuapp.com");
+  }, 300000);
+    
